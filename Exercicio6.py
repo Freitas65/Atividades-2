0326565113a973f729e9e510a5b675b9982c1a65
Gabriel Freitas dos Santos <166877432+Freitas65@users.noreply.github.com>
2026-04-13 11:51:08 -0400

    Delete Exercicio6.py

--- Exercicio6.py
+++
@@ -1,4 +0,0 @@
-horas = int(input("Digite a quantidade de horas: "))
-minutos = int(input("Digite a quantidade de minutos: "))
-total_minutos = (horas * 60) + minutos
-print("O Valor total apenas em minutos é:", total_minutos)
