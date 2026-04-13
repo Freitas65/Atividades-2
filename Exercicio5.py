a14b9cd7593db9344865f887be898e194642cf73
Gabriel Freitas dos Santos <166877432+Freitas65@users.noreply.github.com>
2026-04-13 11:51:00 -0400

    Delete Exercicio5.py

--- Exercicio5.py
+++
@@ -1,6 +0,0 @@
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-nota3 = float(input("Digite a terceira nota: "))
-soma_das_notas = nota1 + nota2 + nota3
-media = soma_das_notas / 3
-print ("A Media do aluno é:", media)
