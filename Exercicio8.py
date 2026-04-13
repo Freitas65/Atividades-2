1ee1f42f7ae08d2f1655bf14b408fb28cc972514
Gabriel Freitas dos Santos <166877432+Freitas65@users.noreply.github.com>
2026-04-13 11:51:23 -0400

    Delete Exercicio8.py

--- Exercicio8.py
+++
@@ -1,5 +0,0 @@
-cateto1 = float(input("Digite o valor do primeiro lado: "))
-cateto2 = float(input("Digite o valor do segundo lado: "))
-soma_quadrados = (cateto1 ** 2)
-hipotenusa = soma_quadrados **0.5
-print("A hipotenusa é", hipotenusa)
