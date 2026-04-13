1494fffbcb4f38b373b17f4911ca723a12cbebaf
Gabriel Freitas dos Santos <166877432+Freitas65@users.noreply.github.com>
2026-04-13 11:50:44 -0400

    Delete Exercicio3.py

--- Exercicio3.py
+++
@@ -1,4 +0,0 @@
-Faltas_1bimestre = int(input("Digite as faltas do 1 Bimestre: "))
-Faltas_2bimestre = int(input("Digite as faltas do 2 Bimestre: 1"))
-total_faltas = Faltas_1bimestre + Faltas_2bimestre
-print ("O total de faltas é:", total_faltas)
